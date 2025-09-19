c7989ad48782430d94d4adc94af960652bddfed3
Sam Lee <sam.lee03@myhunter.cuny.edu>
2025-09-19 12:47:04 -0400

    add search redirect

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,25 @@
 import "./Home.css"
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 function Home () {
-    const [toggleSearch, setToggleSearch] = useState(true)
+    const navigate = useNavigate();
+    
+    const [toggleSearch, setToggleSearch] = useState(true);
+    const [inputValue, setInputValue] = useState('');
+
     const toggleSearchParam = () => {
         setToggleSearch(!toggleSearch);
     }
+    const handleKeyPress = (event : React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            // When Enter is pressed, navigate to the path
+            if (toggleSearch){
+                navigate(`/results/server/${inputValue}`);
+            } else {
+                navigate(`/results/player/${inputValue}`);
+            }
+        }
+    };
     return (
         <>
             <section className="home-header">
@@ -17,7 +32,7 @@ function Home () {
                 <i>Replace with Logo</i>
 
                 {toggleSearch ? <h1>Enter your <strong>server</strong> to get started</h1> : <h1>Find a player</h1>}
-                {toggleSearch ? <input type="text" placeholder="Your server" /> : <input type="text" placeholder="Player name" /> }
+                {toggleSearch ? <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyPress} placeholder="Your server" /> : <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyPress} placeholder="Player name" /> }
                 {toggleSearch ? <button onClick={toggleSearchParam}>I want to find a player in a server</button> : <button onClick={toggleSearchParam}>I'd like to look up a player by name</button>}
             </section>
         </>
--- client/src/pages/ResultsPage/ResultsPage.tsx
+++ client/src/pages/ResultsPage/ResultsPage.tsx
@@ -1,7 +1,11 @@
+import { useParams } from 'react-router-dom'
 function ResultsPage () {
+    const params = useParams();
     return (
         <>
             <h1>Results Page</h1>
+            <p>for {params.category}</p>
+            <p>searched: {params.query}</p>
         </>
     ) 
 }
