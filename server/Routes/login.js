a4051245b4616b1d6b9f920f82c431ddf0ccab9d
alanc224 <alancgc22@gmail.com>
2025-10-14 20:36:25 -0400

    some changes signup is not finished

---
+++ server/Routes/login.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const SECRET_KEY = process.env.SECRET_KEY;
+
+router.post("/login", async(requestAnimationFrame,res) => {
+
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ error: "Both fields are required"})
+        }
+        const user = await User.findOnew({ username });
+
+        if (!user) {
+            return res.send(400).json({ error: "Invalid username or password." })
+        }
+
+        let userFound;
+        try {
+            userFound = await bcrypt.compare(password,user.password);       
+        } catch (bcryptError) {
+            return res.status(500).json({ error: "Server error during login"})
+        }
+        if (!userFound) {
+            return res.status(400).json({ error: "Invalid username or password." });
+        }
+        if (userFound) {
+            const payload = {
+                userId: user._id,
+                username: user.username,
+            };
+            let token;
+            try {
+                // We will be using jwt, signs out in 2 hours we can change if need be
+                token = jwt.sign(payload, SECRET_KEY, { expiresIn: "2h" })
+            } catch (jwtError) {
+                return res.status(500).json({ error: "Server error" })
+            }
+            try {
+                res.cookies('authToken', token, {}) 
+            }
+        }
+    }
+
+
+
+})
