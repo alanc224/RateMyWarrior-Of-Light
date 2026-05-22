4c0b12b10ba6cbf6b4fb3f4d94960a444956c76e
alanc224 <alancgc22@gmail.com>
2026-05-22 17:29:51 -0400

    review stuff

--- server/Models/reviews.js
+++ server/Models/reviews.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
-    hash_user: { type: String, required: true },
+    hash_user: { type: String, required: true, unique: true },
     character_id: { type: String, required: true }, 
     character_name: { type: String, required: true }, 
     server: { type: String, required: true },   
-    rating: { type: Number },
-    comment: { type: String },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
     date: { type: Date, default: Date.now, required: true }
-    // need the boolean questions
+
 });
-reviewSchema.index({ hash_user: 1, character_id: 1 }, { unique: true });
 
 module.exports = mongoose.model('Review', reviewSchema);
