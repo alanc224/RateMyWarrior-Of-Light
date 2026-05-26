e5a699b437def4d9e86c4530c40badee683ca0dd
alanc224 <alancgc22@gmail.com>
2026-05-26 16:59:11 -0400

    fix

--- server/Models/players.js
+++ server/Models/players.js
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
     _id: { type: String, required: true },
-    name: { type: String, required: true },
+    name: String,
     server: String,
     world: String,
-    portrait: String,
+    portrait: String
 }, { timestamps: true });
 
-const Player = mongoose.model('Player', playerSchema);
-export default Player;
+const Character = mongoose.model('Character', playerSchema);
+
+module.exports = Character;
