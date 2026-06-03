9f0e3d597f3cd7d38b543a10c8f8175e75a9c016
alanc224 <alancgc22@gmail.com>
2026-06-03 19:35:10 -0400

    review page css

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,40 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import { SignedIn, SignedOut, UserButton, useClerk } from '@clerk/clerk-react';
 import Logo from "../../assets/homepage/logo.png";
 import './Header.css';
+import WorldSelector from '../WorldSelector/WorldSelector';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [searchName, setSearchName] = useState('');
   const { openSignUp, openSignIn } = useClerk();
+  const [searchWorld, setSearchWorld] = useState('');
+  const isSearchDisabled = !searchName.trim() || !searchWorld;
+
+  const handleSearch = () => {
+    if (searchName) navigate(`/results/player/${searchWorld}/${searchName}`);
+  };
 
   return (
     <div className="header">
       <img className="header-img" src={Logo} onClick={() => navigate('/')} alt="Logo" style={{ cursor: 'pointer' }} />
+
+    <div className="header-search-container">
+        <input 
+          className="header-input" 
+          placeholder="Character name..." 
+          value={searchName} 
+          onChange={(e) => setSearchName(e.target.value)}
+        />
+    
+        <WorldSelector 
+          value={searchWorld} 
+          onChange={(world) => setSearchWorld(world)} 
+        />
+
+        <button className="header-search-btn" onClick={handleSearch} disabled={isSearchDisabled}>Search</button>
+      </div>
     
       <SignedOut>
         <div className="header-buttons-container">
