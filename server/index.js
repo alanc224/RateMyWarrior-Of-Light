4b1167e92b8d6895c075824a1a5955f475c5e4ed
alanc224 <alancgc22@gmail.com>
2026-06-05 13:50:19 -0400

    rate limit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
