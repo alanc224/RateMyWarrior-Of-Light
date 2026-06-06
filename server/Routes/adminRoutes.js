3284ec689cf89f9d024676d7463c0723be7ee8aa
alanc224 <alancgc22@gmail.com>
2026-06-05 21:23:12 -0400

    debugging

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -7,8 +7,6 @@ const ReportModel = require('../Models/Report');
 const ReviewModel = require('../Models/reviews');
 const StatsModel = require('../Models/Stats');
 
-console.log("Authorization Header:", req.headers.authorization);
-
 const checkApiHealth = async () => {
     try {
         const response = await axios.get(process.env.EXTERNAL_API_PROXY);
@@ -20,6 +18,7 @@ const checkApiHealth = async () => {
 
 router.get('/stats', async (req, res) => {
     console.log("FULL SESSION CLAIMS:", JSON.stringify(req.auth?.sessionClaims, null, 2));
+    console.log("Authorization Header:", req.headers.authorization);
     const role = req.auth?.sessionClaims?.role;
     if (role !== 'admin') {
         return res.status(403).json({ error: "Access Denied." });
